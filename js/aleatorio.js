a67bc1e3f913c0cf463e7eb65b0904ff1c9ea094
Camila Paludo <107196927+CamilaPaludo@users.noreply.github.com>
2024-11-08 10:47:39 -0300

    hoje

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,10 @@
+const nomes = ["Camila", "Amanda", "Carla", "Brenda", "Lucas","Tamiris", "José", "Leandro", "Simone"];
+
+
+
 export function aleatorio(lista){
     const posicao = Math.floor(Math.random()*lista.length);
     return lista[posicao];
 }
+
+export const nome = aleatorio(nomes);
